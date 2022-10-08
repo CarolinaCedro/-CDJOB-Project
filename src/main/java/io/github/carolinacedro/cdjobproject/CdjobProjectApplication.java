3daa837276017d1c9b8e84e66e1a25412651c7c3
CarolinaCedro <yotechgirl@gmail.com>
2022-10-07 21:50:52 -0300

    first

---
+++ src/main/java/io/github/carolinacedro/cdjobproject/CdjobProjectApplication.java
@@ -0,0 +1,13 @@
+package io.github.carolinacedro.cdjobproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdjobProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdjobProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/carolinacedro/cdjobproject/CdjobProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.carolinacedro.cdjobproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdjobProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
